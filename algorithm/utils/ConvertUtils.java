eaef4b2106d74067ba51268a8e91d3c1107a1a86
weixuejian <weixuejian@capinfo.com.cn>
2025-09-07 19:32:00 +0800

    feat(ConvertUtils): 添加字符串转二叉树的功能

--- algorithm/utils/ConvertUtils.java
+++ algorithm/utils/ConvertUtils.java
@@ -1,5 +1,8 @@
 package algorithm.utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ConvertUtils {
     // 格式为"[1,2,3,4,5,6,7]"的字符串转为数组
     public static int[] strToIntArray(String str){
@@ -22,6 +25,31 @@ public class ConvertUtils {
         }
         return head;
     }
+    // 格式为"[1,2,3,4,5,6,7]"的字符串转为二叉树
+    public static TreeNode strToTreeNode(String str){
+        int[] arr = strToIntArray(str);
+        TreeNode root = new TreeNode(arr[0]);
+        TreeNode n = root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(n);
+        int i = 1;
+        while(!queue.isEmpty() && i < arr.length-1){
+            n = queue.poll();
+            if("null".equals(arr[i])){
+                n.left = null;
+            }else{
+                n.left = new TreeNode(arr[i]);
+                queue.offer(n.left);
+            }
+            if("null".equals(arr[i+1])){
+                n.right = null;
+            }else{
+                n.right = new TreeNode(arr[i+1]);
+                queue.offer(n.right);
+            }
+            i += 2;
+        }
+        return root;
+    }
 
-    
 }
