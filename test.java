2e6042b1feb6c7902310f581f871ab506bcdbfbe
weixuejian <weixuejian@capinfo.com.cn>
2025-09-09 20:41:10 +0800

    feat: 添加测试代码、算法实现及文档更新

---
+++ test.java
@@ -0,0 +1,10 @@
+import algorithm.utils.ConvertUtils;
+import algorithm.utils.Print;
+
+public class test {
+    public static void main(String[] args) {
+        int[] num1 = ConvertUtils.strToIntArray("[1,2,3,2,1]");
+        int[] num2 = ConvertUtils.strToIntArray("[3,2,1,4,7]");
+        // Print.print();
+    }
+}
